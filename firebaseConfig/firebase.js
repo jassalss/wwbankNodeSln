5ec0e6b36c8a4bbad17ac44f59991cb92c87f4b8
Simerpreet S Jassal <simerpreetjassal13@gmail.com>
2020-11-10 22:08:28 -0700

    removed api keys

--- firebaseConfig/firebase.js
+++ firebaseConfig/firebase.js
@@ -1,12 +1,12 @@
 var firebase = require("firebase");
 var firebaseConfig = {
-  apiKey: "AIzaSyAKcJNiSPu-T4hKcwlv0wuSg5TJ-Ma5Dg8",
-  authDomain: "wwbankcanada.firebaseapp.com",
-  databaseURL: "https://wwbankcanada.firebaseio.com",
-  projectId: "wwbankcanada",
-  storageBucket: "wwbankcanada.appspot.com",
-  messagingSenderId: "682643586198",
-  appId: "1:682643586198:web:0caef574190cd7d9fc56b7",
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 firebase.initializeApp(firebaseConfig);
 exports.realTimeDB = firebase.database();
